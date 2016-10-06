21f7c0bf317b3ff8462a3a2a9d22325bdbff04d8
AlexeyTsy <alex.ts.mobile@gmail.com>
2016-10-06 17:07:41 +0300

    done fird task
    done fird task

--- scriptdz4.js
+++ scriptdz4.js
@@ -26,4 +26,23 @@ function countBs(a, b){
 countBs(char, string);
 console.log(count);
 
+////////////Задание 2///////////////////////////////////////////////////
 
+function isEven(n){
+    if (n == 0){
+        return true;
+    } else if (n < 0) {
+        return isEven(n + 2);
+    } else if(n == 1) {
+        return false;
+    } else {
+        return isEven(n-2);
+    }
+}
+
+console.log(isEven(50));
+console.log(isEven(75));
+console.log(isEven(0));
+console.log(isEven(-1));
+console.log(isEven(-10));
+console.log(isEven(-75));
