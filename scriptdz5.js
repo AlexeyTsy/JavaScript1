2b2240136220e82cf70f504216ce0b4f0a600484
AlexeyTsy <alex.ts.mobile@gmail.com>
2016-10-08 09:41:25 +0300

    Задание 5
    Задание 5

--- scriptdz5.js
+++ scriptdz5.js
@@ -1,3 +1,18 @@
 /**
  * Created by AlexKate on 10/8/2016.
  */
+
+function transform(a, b, c) {
+    if ((a < 0) && (a > 255)) {
+       console.log("Неверно введеное число!");
+    }
+    if ((b < 0) && (b > 255)) {
+        console.log("Неверно введеное число!");
+    }
+    if ((c < 0) && (c > 255)) {
+        console.log("Неверно введеное число!");
+    }
+    console.log("#" + a.toString(16) + b.toString(16) + c.toString(16));
+}
+
+transform(15, 15, 15);
